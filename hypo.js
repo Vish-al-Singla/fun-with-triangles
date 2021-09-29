b60856a645033b8c827a247c10cdbb817fd630cb
Vishal <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-09-29 22:15:10 +0530

    Added js file

---
+++ hypo.js
@@ -0,0 +1,11 @@
+const sides = document.querySelectorAll('.side-input');
+const hypotenuseBtn = document.querySelector('#hypotenuse-btn');
+const outputEl = document.querySelector('#output');
+
+
+function calculateHypotenuse (){
+    
+}
+
+
+hypotenuseBtn.addEventListener("click",calculateHypotenuse);
