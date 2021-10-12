d11167488f21aebf75bd7b587fb57479cbdba79a
Vishal <s.i.n.g.l.a.v.i.s.h.a.l.s.v@gmail.com>
2021-10-12 13:03:54 +0530

    minor changes exit x xit() exit()

--- isTriangle.js
+++ isTriangle.js
@@ -3,15 +3,25 @@ const isTriangleBtn = document.querySelector('#is-triangle-btn');
 const isTriangleOutput = document.querySelector('#is-triangle-output');
 
 function calculateSumOfAngles() {
-	const sumOfAngles =
-		Number(angles[0].value) + Number(angles[1].value) + Number(angles[2].value);
+	const angle1 = Number(angles[0].value);
+	const angle2 = Number(angles[1].value);
+	const angle3 = Number(angles[2].value);
+	const sumOfAngles = angle1 + angle2 + angle3;
 	console.log(sumOfAngles);
 	return sumOfAngles;
 }
 
 function isTriangle() {
-	if(angles>0)
+	const angle1 = Number(angles[0].value);
+	const angle2 = Number(angles[1].value);
+	const angle3 = Number(angles[2].value);
+	if(angle1<0 || angle2<0|| angle3<0)
 	{
+	
+		isTriangleOutput.innerText = 'Enter valid inputs!!'
+		
+	}
+	else{
 		if (calculateSumOfAngles() === 180) {
 			console.log('Yayy, the angles form a triangle');
 			isTriangleOutput.innerText = 'Yayy, the angles form a triangle';
@@ -26,11 +36,7 @@ function isTriangle() {
 				isTriangleOutput.innerText = 'Oh Oh!, the angles do not form a triangle';
 			}
 	}
-	
 		
-	}
-	else{
-		isTriangleOutput.innerText = 'Enter valid inputs!!'
 	}
 }
 isTriangleBtn.addEventListener('click', isTriangle);
